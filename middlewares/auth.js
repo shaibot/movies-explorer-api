90d3ef10b6c29ea3c4d826a580180aee41809c3b
Valentina Bogdanova <zh-vln@yandex.ru>
2023-08-03 19:07:40 +0300

    refactoring api

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,24 +3,20 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET, NODE_ENV } = process.env;
 const Unauthorized = require('../Errors/Unauthorized');
 const { DEV_SECRET, NODE_PRODUCTION } = require('../config');
+const { log } = require('winston');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const { cookie } = req.headers;
+  if (!cookie) {
     return next(new Unauthorized('Необходимо пройти авторизацию'));
   }
-
-  const token = authorization.replace('Bearer ', '');
+  const token = cookie.replace('jwt=', '');
   let payload;
-
   try {
     payload = jwt.verify(token, NODE_ENV === NODE_PRODUCTION ? JWT_SECRET : DEV_SECRET);
   } catch (err) {
     return next(new Unauthorized('Необходимо пройти авторизацию'));
   }
-
   req.user = payload;
-
   return next();
 };
